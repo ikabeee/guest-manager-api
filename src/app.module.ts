a36fd1f2de1a3f43f724244f74e143f5f3172326
ikabeee <carlglz30@gmail.com>
2025-02-19 12:59:48 -0500

    add user module with controller and service; implement create, read, update, and delete functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }), UserModule],
   controllers: [],
   providers: [],
 })
