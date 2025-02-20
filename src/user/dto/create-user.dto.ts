f2f6636a3a9ca5bf04205a57d6d34f0e6ad54da7
ikabeee <carlglz30@gmail.com>
2025-02-19 19:07:06 -0500

    add user role enum and enhance CreateUserDto validation; update user controller and service methods for improved error handling and routing

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Role } from 'src/common/enums/Role.enum';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  @IsNotEmpty()
+  tel: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  sites: number;
+
+  @IsEnum(Role)
+  @IsNotEmpty()
+  role: Role;
+}
