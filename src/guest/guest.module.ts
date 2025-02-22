e50f3f7c0a57fe29d4e903833ac395de42c4bb87
ikabeee <carlglz30@gmail.com>
2025-02-21 21:57:56 -0500

    update dependencies to @prisma/client and prisma version 6.4.1; import PrismaModule in GuestModule

--- src/guest/guest.module.ts
+++ src/guest/guest.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GuestService } from './guest.service';
 import { GuestController } from './guest.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [GuestController],
   providers: [GuestService],
+  imports: [PrismaModule],
 })
 export class GuestModule {}
